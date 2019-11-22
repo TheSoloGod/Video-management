f1a78bde974532581dd0099298d149fe826d9bd4
TheSoloGod <kienaof94@gmail.com>
2019-11-22 15:06:02 +0700

    refactor route auth default laravel

--- routes/front-end.php
+++ routes/front-end.php
@@ -13,9 +13,6 @@
 
 //ROUTE FRONT END
 
-//verify email
-Auth::routes(['verify' => true]);
-
 // index
 Route::get('/', function () {
     return redirect()->route('home.public.index');
@@ -23,6 +20,27 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('front_end.member.home-new');
 })->name('home.new');
+
+//verify email
+Auth::routes(['verify' => true]);
+Route::group(['verify' => true], function () {
+    // Authentication Routes...
+    Route::get('login', 'Frontend\Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Frontend\Auth\LoginController@login');
+    Route::post('logout', 'Frontend\Auth\LoginController@logout')->name('logout');
+    // Registration Routes...
+    Route::get('register', 'Frontend\Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Frontend\Auth\RegisterController@register');
+    // Password Reset Routes...
+    Route::get('password/reset', 'Frontend\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Frontend\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Frontend\Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Frontend\Auth\ResetPasswordController@reset')->name('password.update');
+    // Email Verification Routes...
+    Route::get('email/verify', 'Frontend\Auth\VerificationController@show')->name('verification.notice');
+    Route::get('email/verify/{id}', 'Frontend\Auth\VerificationController@verify')->name('verification.verify');
+    Route::get('email/resend', 'Frontend\Auth\VerificationController@resend')->name('verification.resend');
+});
 
 // route public
 Route::group(['prefix' => 'public'], function () {
@@ -48,12 +66,14 @@ Route::get('/search', 'Backend\Video\VideoController@search')->name('navbar.sear
 Route::get('/category/{category_id}', 'Backend\Video\VideoController@getPaginateVideoOfCategory')->name('category.filter');
 
 
+
 //ROUTE TEST
 
 //route test
 Route::get('test', function () {
    return view('test-vue');
 })->name('test');
+//route api vuejs
 Route::resource('products', 'Backend\Product\ProductController');
 
 
